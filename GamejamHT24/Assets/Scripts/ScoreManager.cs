f7d0ad90c1dae342a7e7df1cad903d645a1f1c97
87ch87 <ch.huusko@gmail.com>
2024-10-08 09:40:14 +0200

    ScoreManager

---
+++ GamejamHT24/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance; //Do ScoreManager.instance.AddPoint(); in the method for completing an order
+
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = "SCORE: " + score.ToString();
+        highscoreText.text = "HIGHSCORE: " + highscore.ToString();
+    }
+
+    public void AddScore()
+    {
+        score += 1; //Change to correct value for order completion
+        scoreText.text = "SCORE: " + score.ToString();
+        if (highscore < score)
+        {
+            PlayerPrefs.SetInt("highscore", score);
+        }
+    }
+}
