f4b7be6ea5d05957376fea60821e5fbbfcbf3946
Joppa0 <114592889+Joppa0@users.noreply.github.com>
2024-10-09 12:16:45 +0200

    Changed spawn point difficulty
    - The amount of valid spawn points now increase over time, starting from 1.

--- GamejamHT24/Assets/Scripts/TrayManager.cs
+++ GamejamHT24/Assets/Scripts/TrayManager.cs
@@ -11,12 +11,14 @@ public class TrayManager : MonoBehaviour
     private float baseSpawnInterval = 5f; 
     private bool[] isLocationOccupied; 
     private Dictionary<GameObject, int> trays = new Dictionary<GameObject, int>();
+    private int spawnPointAmount = 1;
 
     void Start()
     {
         isLocationOccupied = new bool[spawnLocations.Length];
         SpawnTrayIfSpotAvailable();
         StartCoroutine(SpawnTrays());
+        StartCoroutine(IncreaseSpawnPoints());
     }
 
     IEnumerator SpawnTrays()
@@ -26,6 +28,19 @@ public class TrayManager : MonoBehaviour
             float spawnInterval = Mathf.Max(2f, baseSpawnInterval - gameClock.timePassed / 60f);
             yield return new WaitForSeconds(spawnInterval);
             SpawnTrayIfSpotAvailable();
+        }
+    }
+
+    // The amount of available spawn points increases over time to make the game more difficult.
+    IEnumerator IncreaseSpawnPoints()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(15);
+            spawnPointAmount++;
+
+            if (spawnPointAmount >= spawnLocations.Length)
+                break;
         }
     }
 
@@ -61,7 +76,7 @@ public class TrayManager : MonoBehaviour
     int[] GetAvailableIndices()
     {
         var availableIndices = new System.Collections.Generic.List<int>();
-        for (int i = 0; i < spawnLocations.Length; i++)
+        for (int i = 0; i < spawnPointAmount; i++)
         {
             if (!isLocationOccupied[i])
             {
